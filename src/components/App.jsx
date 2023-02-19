28e46ccf23cf343d87d8c786ae227ac25fd3d020
Mari <mrs.tereshchenko@gmail.com>
2023-02-19 12:33:46 +0100

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,28 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './Navbar/Navbar';
-import MoviesPage from './Pages/MoviesPage';
-import HomePage from './Pages/HomePage';
-import MovieDetailsPage from './Pages/MovieDetailsPage';
-import MovieCastPage from './Pages/MovieCast';
-import MovieReviewsPage from './Pages/MovieReviews';
+import { lazy, Suspense } from 'react';
+
+const Navbar = lazy(() => import('./Navbar/Navbar'));
+const MoviesPage = lazy(() => import('./Pages/MoviesPage'));
+const HomePage = lazy(() => import('./Pages/HomePage'));
+const MovieDetailsPage = lazy(() => import('./Pages/MovieDetailsPage'));
+const MovieCastPage = lazy(() => import('./Pages/MovieCast'));
+const MovieReviewsPage = lazy(() => import('./Pages/MovieReviews'));
 
 export const App = () => {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/movies" element={<MoviesPage />} />
-        <Route path="/movies/:id" element={<MovieDetailsPage />}>
-          <Route path="cast" element={<MovieCastPage />} />
-          <Route path="reviews" element={<MovieReviewsPage />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<p>...loading</p>}>
+        <Routes>
+          <Route path="/" element={<Navbar />}>
+            <Route index element={<HomePage />} />
+            <Route path="/movies" element={<MoviesPage />} />
+            <Route path="/movies/:id" element={<MovieDetailsPage />}>
+              <Route path="cast" element={<MovieCastPage />} />
+              <Route path="reviews" element={<MovieReviewsPage />} />
+            </Route>
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,31 @@
 import { NavLink } from 'react-router-dom';
 import styles from './navbar.module.css';
+import { Outlet } from 'react-router-dom';
+import { Suspense } from 'react';
 
 const Navbar = () => {
   return (
-    <>
-      <ul className={styles.navmenu}>
-        <li>
-          <NavLink to="/" className={styles.link}>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/movies" className={styles.link}>
-            Movies
-          </NavLink>
-        </li>
-      </ul>
-    </>
+    <section>
+      <header>
+        <ul className={styles.navmenu}>
+          <li>
+            <NavLink to="/" className={styles.link} end>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/movies" className={styles.link}>
+              Movies
+            </NavLink>
+          </li>
+        </ul>
+      </header>
+      <Suspense fallback={<div>Loading...</div>}>
+        <main>
+          <Outlet />
+        </main>
+      </Suspense>
+    </section>
   );
 };
 
